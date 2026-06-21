97f5fbdad2281a45eee500605dd47e918b8b66ca
chief575012 <chief5750@strayhood.org>
2026-06-21 23:52:32 +0700

    Update MainSource.lua

--- TDS/MainSource.lua
+++ TDS/MainSource.lua
@@ -587,11 +587,11 @@ if CheckPlace() then
 	StratXLibrary.ReadyState = false
 	StratXLibrary.VoteState = VoteGUI:GetAttributeChangedSignal("Enabled"):Connect(function()
 	-- Is it MaxVotes Or What??
-	if VoteGUI:GetAttribute("MaxVotes") ~= #Players:GetPlayers() then
+	--[[if VoteGUI:GetAttribute("MaxVotes") ~= #Players:GetPlayers() then
 			repeat
                 task.wait()
             until VoteGUI:GetAttribute("MaxVotes") == #Players:GetPlayers()
-		end
+		end]]
 		
 		local currentPrompt = VoteGUI:GetAttribute("Title")
    		if currentPrompt == "Ready?" or currentPrompt == "Skip Cutscene?" then --Event GameMode
