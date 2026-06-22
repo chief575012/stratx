01a44c19abf40d766932c1e391085a57e8b2a9bc
chief575012 <chief5750@strayhood.org>
2026-06-22 18:45:28 +0700

    Check if game is over before restarting count

--- TDS/MainSource.lua
+++ TDS/MainSource.lua
@@ -740,7 +740,7 @@ if CheckPlace() then
 			warn("Connection Ran!?")
 			prints("GameOver Changed")
 			local Remote
-			if not GameState:GetAttribute("GameOver") then
+			if not Gameoveryet() then
 				return
 			end
 			StratXLibrary.RestartCount += 1 --need to stop handler, timewavewait
